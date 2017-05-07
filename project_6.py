e2c2c9017634dd8e582dfc9012e0024bd01b75c3
Jonathan Speek <jonspeek@gmail.com>
2017-05-07 14:46:55 -0600

    Finished project 6

--- project_6.py
+++ project_6.py
@@ -6,3 +6,13 @@
 # should be in tabular format with the appropriate header. Each line should contain
 # an employee’s name, the hours worked, and the wages paid for that period.
 
+payroll_file = open('project_6.txt', 'r')
+
+print('Name\tWage\tHours\tPay')
+for row in payroll_file:
+    person = row.split('\t')
+    last_name = person[0]
+    wage = float(person[1])
+    hours = int(person[2][:-2])
+    pay = round(wage * hours, 2)
+    print('{name}\t{wage}\t{hours}\t{pay}\n'.format(name=last_name, wage=wage, hours=hours, pay=pay))
