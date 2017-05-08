2a07889d4e3d559ad508e97e022bed67f30bbf97
Jonathan Speek <jonspeek@gmail.com>
2017-05-07 19:02:30 -0600

    moving on...

--- project_10.py
+++
@@ -1,4 +0,0 @@
-# Develop a Library class that can manage the books and patrons from Project 9.
-# This class should include methods for adding, removing, and finding books and
-# patrons. There should also be methods for borrowing and returning a book. Write
-# a script to test all these methods.
